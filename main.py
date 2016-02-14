23e7140176ff795defac998bd5c109a5426f5caa
Paul Savala <paulsavala@gmail.com>
2016-02-14 09:38:46 -0600

    Added hill and variability scores

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from stravalib import Client, unithelper
 import pandas as pd
 from bokeh.plotting import figure, output_file, show
 from bokeh.embed import components
+import datetime
 
 app_lulu = Flask(__name__)
 
@@ -24,6 +25,60 @@ def stream_to_df(stream):
 	
 	return stream_df
 
+# Compute the variability scores
+def variability_scores(grad_series):
+	# Separate into positive and negative gradients
+	grad_pos = grad_series[grad_series >= 0]
+	grad_neg = grad_series[grad_series < 0]
+
+	# Compute the means
+	grad_mean = grad_series.mean()
+	grad_pos_mean = grad_pos.mean()
+	grad_neg_mean = grad_neg.mean()
+
+	# Get the lengths
+	n = grad_series.shape[0]
+	n_pos = grad_pos.shape[0]
+	n_neg = grad_neg.shape[0]
+
+	# Compute the positive and negative variability scores
+	try:
+		var_pos = math.sqrt(sum((grad_pos - grad_pos_mean) ** 2)) / n_pos
+	except ZeroDivisionError:
+		var_pos = 0
+
+	try:
+		var_neg = math.sqrt(sum((grad_neg - grad_neg_mean) ** 2)) / n_neg
+	except ZeroDivisionError:
+		var_neg = 0
+
+	# Compute total variability
+	var = var_pos + var_neg
+	
+	return var, var_pos, var_neg
+	
+# Grades a hill according to how uphill / downhill it is
+# "Large" positive values been long, steep climbs
+# "Large" negative values mean long, steep descents
+def hill_score(grad_series, dist_series):
+	# We assume that grad_series is a pandas series holding the gradients
+	# at each measurement, and that dist_series is the same for distances 
+	# (measured at the same time)
+	dist_diff = dist_series.diff().drop(0)
+	grad_series = grad_series.drop(0)
+	
+	hill_score = sum(dist_diff * grad_series)
+	
+	return hill_score
+
+# Grab the most recent activities
+def recent_activities(num_activities = 5):
+	client = Client(access_token = app_lulu.vars['access_token'])
+	athlete = client.get_athlete()
+	
+	activities = client.get_activities(limit = num_activities)
+	
+	return activities
 # ==================- Flask functions -=====================
 
 # Have user connect with Strava
@@ -39,27 +94,15 @@ def authorization():
 	
 	code = request.args.get('code')
 	
-	access_token = client.exchange_code_for_token(client_id = my_strava_client_id, \
+	access_token = client.exchange_code_for_token(client_id = my_client_id, \
 		client_secret = my_client_secret, code = code)
 	app_lulu.vars['access_token'] = access_token
 	
-	return redirect('/segment_stream')
-
-# Get the users recent activities
-@app_lulu.route('/recent_activities')
-def recent_activities():
-	num_activities = 5
-	client = Client(access_token = app_lulu.vars['access_token'])
-	athlete = client.get_athlete()
-	
-	activities = client.get_activities(limit = num_activities)
-	
-	return 'Got the 5 most recent activities'
+	return redirect('/graph_profile')
 
 # Get the stream for a SEGMENT
 @app_lulu.route('/segment_stream')
-def segment_stream():
-	segment_id = 3944715
+def segment_stream(segment_id = 3944715):
 	types = ['distance', 'altitude']
 	stream = client.get_segment_streams(segment_id, types = types, resolution = 'high')
 
@@ -70,7 +113,9 @@ def segment_stream():
 # Render the power profile chart page. Right now the chart is static.
 @app_lulu.route('/graph_profile')
 def graph_profile():
-
+	curr_athlete = client.get_athlete()
+	
+	# This is a temporary, static graph acting as a placeholder
 	p = figure(plot_width = 450, plot_height = 450)
 	p.logo = None
 	p.toolbar_location = None
@@ -81,9 +126,20 @@ def graph_profile():
 	
 	script, div = components(p)
 	
-	return render_template('layout.html', script = script, div = div)
+	# Next we grab the most recent rides
+	recent_rides = recent_activities()
+	
+	return render_template('layout.html', script = script, div = div, recent_activities = recent_rides, athlete = curr_athlete)
+	
+# ======================- Jinja filters -========================= 
+# @app_lulu.template_filter('strftime')
+# def _jinja2_filter_date(d, fmt = None):
+#     date = datetime.datetime.strptime(d, fmt)
+#     native = date.replace(tzinfo = None)
+#     format='%Y'
+#     return native.strftime(format) 
 
-# ======================- Housekeeping functions -=========================
+# ======================- Housekeeping functions -================
 
 # Settings for local Flask deployment (for testing)
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -2,7 +2,58 @@ import pandas as pd
 import numpy as np
 from bokeh.plotting import figure, output_file, show
 from bokeh.embed import components
+import math
+import sqlite3
 
+# ===============- Variability score -=======================
+def variability_scores(grad_series):
+	# Separate into positive and negative gradients
+	grad_pos = grad_series[grad_series >= 0]
+	grad_neg = grad_series[grad_series < 0]
+
+	# Compute the means
+	grad_mean = grad_series.mean()
+	grad_pos_mean = grad_pos.mean()
+	grad_neg_mean = grad_neg.mean()
+
+	# Get the lengths
+	n = grad_series.shape[0]
+	n_pos = grad_pos.shape[0]
+	n_neg = grad_neg.shape[0]
+
+	# Compute the positive and negative variability scores
+	try:
+		var_pos = math.sqrt(sum((grad_pos - grad_pos_mean) ** 2)) / n_pos
+	except ZeroDivisionError:
+		var_pos = 0
+
+	try:
+		var_neg = math.sqrt(sum((grad_neg - grad_neg_mean) ** 2)) / n_neg
+	except ZeroDivisionError:
+		var_neg = 0
+
+	# Compute total variability
+	var = var_pos + var_neg
+	
+	return var_pos, var_neg, var
+
+# ====================- Hill score -===========================
+# Grades a hill according to how uphill / downhill it is
+# "Large" positive values been long, steep climbs
+# "Large" negative values mean long, steep descents
+def hill_score(grad_series, dist_series):
+	# We assume that grad_series is a pandas series holding the gradients
+	# at each measurement, and that dist_series is the same for distances 
+	# (measured at the same time)
+	dist_diff = dist_series.diff().drop(0)
+	grad_series = grad_series.drop(0)
+	
+	hill_score = sum(dist_diff * grad_series)
+	
+	return hill_score
+	
+
+# ===================- Data -==================================
 short_df = pd.DataFrame.from_csv('stream.csv')
 short_grad_df = short_df.diff().altitude / short_df.diff().distance
 short_df['gradient'] = short_grad_df.replace(np.nan, 0)
@@ -11,13 +62,30 @@ long_df = pd.DataFrame.from_csv('stream2.csv')
 long_grad_df = long_df.diff().altitude / long_df.diff().distance
 long_df['gradient'] = long_grad_df.replace(np.nan, 0)
 
-output_file('patch_bokeh.html')
-p = figure()
-p.patch([-3,0,2,0], [0,2,0,-1], line_width = 2, alpha = 0.5)
-p.grid.bounds = (0, 0)
-p.axis.minor_tick_line_color = None
-p.xaxis.axis_label = ''
-p.yaxis.axis_label = ''
-show(p)
 
-script, div = components(p)
+
+var_pos, var_neg, var = variability_scores(short_df['gradient'])
+print('Var+ = %f' % var_pos)
+print('Var- = %f' % var_neg)
+print('Var = %f' % var)
+print('Hill score = %f' % hill_score(short_df['gradient'], short_df['distance']))
+print('=' * 20)
+
+var_pos, var_neg, var = variability_scores(long_df['gradient'])
+print('Var+ = %f' % var_pos)
+print('Var- = %f' % var_neg)
+print('Var = %f' % var)
+print('Hill score = %f' % hill_score(long_df['gradient'], long_df['distance']))
+
+
+# ====================- Formatting datetimes -====================
+# import datetime
+# now = datetime.date.today()
+# 
+# def filter_datetime(date, fmt=None):
+#     date = dateutil.parser.parse(date)
+#     native = date.replace(tzinfo=None)
+#     format='%b %d, %Y'
+#     return native.strftime(format) 
+    
+print(filter_datetime(now))
